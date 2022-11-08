b704a622c031b2e010c50ae73b251f300450f214
nico <westernfarthing@gmail.com>
2022-11-08 00:45:21 -0600

    feat: add javaScript logic, update deployed app image

--- src/app/script.js
+++ src/app/script.js
@@ -1,5 +1,65 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+// query selectors
+var generateBtn = document.querySelector("#generate")
+
+function generatePassword(){
+  // special characters
+  var specSymbols = ["!", "\"", "#", "$", "%", "&", "'", "(", ")","*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@","[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+  var numbers = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9"]
+  var lowerCaseCharset = Object.assign([],"abcdefghijklmnopqrstuvwxyz")
+  var upperCaseCharset = Object.assign([],"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+  var passLength = 0
+  alert("Let's generate a secure password!" );
+  passLength = prompt("How long should the password be? Choose length 8-128 chars"); 
+
+  // user input prompts and confirmations
+  if((passLength < 7) || ( passLength > 129)) {
+      alert("Please enter a valid password")
+      console.log("you done messed up, RETRY WITH A VALID NUMBER!!!!")
+  }else {
+      console.log("great")
+      confirm("you entered " + passLength + " as your password length. IS THIS CORRECT??!!!")
+      var lowerCaseTrue = confirm("Do you want to include LOWERCASE characters???!!!!")
+      console.log(lowerCaseTrue)
+      // include uppercase??
+      var upperCaseTrue = confirm("Do you want to include UPPERCASE characters???!!!!!")
+      console.log(upperCaseTrue)
+      // include numerics??
+      var numbersTrue = confirm("Do you want to include NUMBERS???!!!!!!!")
+      console.log(numbersTrue)
+      // include special characters??
+      var specialCharsTrue = confirm("Do you want to include SPECIAL CHARACTERS???!!!!!!!")
+      console.log(specialCharsTrue)
+
+      var passwordSet = []
+
+//      console.log(passwordSet)
+
+      if(lowerCaseTrue === true) {
+        passwordSet = passwordSet.concat(lowerCaseCharset)
+      }
+      if(upperCaseTrue === true) {
+        passwordSet = passwordSet.concat(upperCaseCharset)
+      }
+      if(numbersTrue === true) {
+        passwordSet = passwordSet.concat(numbers)
+      }
+      if(specialCharsTrue === true) {
+        passwordSet = passwordSet.concat(specSymbols)
+      }
+    // input should be validated and at least one character type should be selected
+      if(passwordSet.length === 0) {
+      alert("PICK SOMETHING TO BUILD A PASSWORD!!!!!!")
+      return
+      }
+    // Add selected chars and numbers to final password selection array
+    var password = ""
+    for (let index = 0; index < passLength; index++) {
+      password += passwordSet[Math.floor(Math.random()*passwordSet.length)]
+    }   
+  } 
+    return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +67,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+// Interactive event
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword)
